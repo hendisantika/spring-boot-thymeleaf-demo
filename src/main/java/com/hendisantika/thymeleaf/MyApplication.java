24bcf6fa1075e7a9115dc6d11956a9b13ae70176
Hendi Santika <hendisantika@gmail.com>
2017-12-09 06:13:18 +0700

    #2 - Add Main App

---
+++ src/main/java/com/hendisantika/thymeleaf/MyApplication.java
@@ -0,0 +1,22 @@
+package com.hendisantika.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/12/17
+ * Time: 18.28
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}            
