ed481369e58072528c16277f11e8aa5126e3679c
Hendi Santika <hendisantika@gmail.com>
2017-12-10 06:37:15 +0700

    #11 - Add Index Controller

--- src/main/java/com/hendisantika/thymeleaf/controller/IndexController.java
+++ src/main/java/com/hendisantika/thymeleaf/controller/IndexController.java
@@ -1,5 +1,12 @@
 package com.hendisantika.thymeleaf.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-demo
@@ -10,5 +17,15 @@ package com.hendisantika.thymeleaf.controller;
  * Time: 06.25
  * To change this template use File | Settings | File Templates.
  */
+
+@Controller
+@RequestMapping("")
 public class IndexController {
+    @GetMapping()
+    String index(Model m){
+        m.addAttribute("now", new Date());
+        m.addAttribute("nama", "Hendi Santika");
+        return "index";
+    }
+
 }
