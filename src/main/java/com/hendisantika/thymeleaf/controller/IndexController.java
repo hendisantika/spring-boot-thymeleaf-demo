a33f96f21d3e71592e160c0fd191eac4631269e6
Hendi Santika <hendisantika@gmail.com>
2017-12-10 06:36:56 +0700

    #10 - Add thymeleaf config

---
+++ src/main/java/com/hendisantika/thymeleaf/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.hendisantika.thymeleaf.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/12/17
+ * Time: 06.25
+ * To change this template use File | Settings | File Templates.
+ */
+public class IndexController {
+}
